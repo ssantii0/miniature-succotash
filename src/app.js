76ce0349062131d3305edcf6b8b51a8a7e6f2e72
ssantii0 <santitom2004@gmail.com>
2024-09-27 17:58:51 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const quien = [" el perro ", " mi abuela ", " mi pajaro ", " el cartero"];
+  const hizo = [" se comió ", " rompió ", " tiró ", " perdió "];
+  const que = [" mis deberes ", " mi movil ", " mi cartera "];
+  const cuando = [
+    " despues de clases. ",
+    " cuando yo estaba durmiedo. ",
+    " mientras comía. ",
+    " mientras yo estaba jugando. ",
+    " mientras hacia ejercicio. "
+  ];
+
+  function generarExcusa() {
+    const Sujeto = quien[Math.floor(Math.random() * quien.length)];
+    const Accion = hizo[Math.floor(Math.random() * hizo.length)];
+    const Objeto = que[Math.floor(Math.random() * que.length)];
+    const Tiempo = cuando[Math.floor(Math.random() * cuando.length)];
+
+    let respuestaFinal = ` ${Sujeto} ${Accion} ${Objeto} ${Tiempo}`;
+    document.getElementById("respuesta").innerText = respuestaFinal;
+  }
+  document.getElementById("generador").addEventListener("click", () => {
+    generarExcusa();
+  });
 };
